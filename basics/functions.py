5392878f9051d52a34677fa7e639e60d2631018b
git-ankur-shukla <work.ankurshukla@gmail.com>
2019-05-21 13:10:21 +0530

    working on day2

--- basics/functions.py
+++ basics/functions.py
@@ -1,14 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def stretch(image):
-    temp = image
-    for i in range(image.shape[2]):
-        minI    = np.min(image[:,:,i])
-        maxI    = np.max(image[:,:,i])
+def stretch(image, mode = 'linear'):
+    temp = image.copy()
+    if(len(temp.shape) == 3):
+        for i in range(image.shape[2]):
+            minI    = np.min(image[:,:,i])
+            maxI    = np.max(image[:,:,i])
+            minO    = 0
+            maxO    = 255
+
+            temp[:,:,i] = (image[:,:,i]-minI)*(((maxO-minO)/(maxI-minI))+minO)
+    else:
+        minI    = np.min(image)
+        maxI    = np.max(image)
         minO    = 0
         maxO    = 255
 
-        temp[:,:,i] = (image[:,:,i]-minI)*(((maxO-minO)/(maxI-minI))+minO)
-    
+        temp = (image-minI)*(((maxO-minO)/(maxI-minI))+minO)
     return temp
+
+
